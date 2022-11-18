aa51ca6b6516ea201cb1a4addad5e71f40a1aacf
Pavel <pavel.holod2001@gmail.com>
2022-11-18 04:01:17 +0300

    f changeCost

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
 const slider = document.querySelector('#range')
+const pageviews = document.querySelector('#pageviews')
+const cost = document.querySelector('#cost')
+const switchBtn = document.querySelector('#switch')
 
 const views = ['10K', '50K', '100K', '500K', '1M']
 const price = [8, 12, 16, 24, 36]
 
+slider.max = views.length - 1
 
 slider.addEventListener('input', chengeSlider)
 function chengeSlider() {
     const value = slider.value
     slider.style.background = `linear-gradient(90deg, var(--cyan-100) ${value * 25}%, var(--blue-200) ${value}%)`
+    changeCost()
+}
+
+function changeCost() {
+    const value = slider.value
+    const chk = switchBtn.checked
+    if (chk) {
+        pageviews.innerText = `${views[value]} Pageviews`
+        cost.innerHTML = `$${price[value] * 12 * .75}.00<span>/year</span>`
+    } else {
+        pageviews.innerText = `${views[value]} Pageviews`
+        cost.innerHTML = `$${price[value]}.00<span>/month</span>`
+    }
 }
