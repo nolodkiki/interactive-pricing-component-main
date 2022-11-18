caa68c70cc7cef191adc081cea7faabb1ab9c9ef
Pavel <pavel.holod2001@gmail.com>
2022-11-18 05:32:27 +0300

    optimization

--- main.js
+++ main.js
@@ -8,21 +8,27 @@ const price = [8, 12, 16, 24, 36]
 
 slider.max = views.length - 1
 
+// ================slider================
 slider.addEventListener('input', chengeSlider)
 function chengeSlider() {
     const value = slider.value
     slider.style.background = `linear-gradient(90deg, var(--cyan-100) ${value * 25}%, var(--blue-200) ${value}%)`
     changeCost()
 }
+// ================slider================
 
 function changeCost() {
-    const value = slider.value
     const chk = switchBtn.checked
+    const yearCost = 12 * .75
     if (chk) {
-        pageviews.innerText = `${views[value]} Pageviews`
-        cost.innerHTML = `$${price[value] * 12 * .75}.00<span>/year</span>`
+        inner(yearCost)
     } else {
-        pageviews.innerText = `${views[value]} Pageviews`
-        cost.innerHTML = `$${price[value]}.00<span>/month</span>`
+        inner(1)
     }
+}
+
+function inner(num) {
+    const value = slider.value
+    pageviews.innerText = `${views[value]} Pageviews`
+    cost.innerHTML = `$${price[value] * num}.00<span>/month</span>`
 }
