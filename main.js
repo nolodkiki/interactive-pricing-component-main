22d579aab9db841c6b68b143987a832f50ff1908
Pavel <pavel.holod2001@gmail.com>
2022-11-18 03:20:50 +0300

    f style-guide.md

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const views = ['10K', '50K', '100K', '500K', '1M']
 const price = [8, 12, 16, 24, 36]
 
 
-slider.addEventListener('input', chengeSlider())
+slider.addEventListener('input', chengeSlider)
 function chengeSlider() {
     const value = slider.value
-    slider.style.background = `linear-gradient(90deg, var(--cyan-100) ${slider[value]}%, var(--blue-200) ${slider[value]}%)`
+    slider.style.background = `linear-gradient(90deg, var(--cyan-100) ${value * 25}%, var(--blue-200) ${value}%)`
 }
