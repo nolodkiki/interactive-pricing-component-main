a09d3569c2dd8491040bca0c104ae581132d5247
Pavel <pavel.holod2001@gmail.com>
2022-11-18 03:13:47 +0300

    01

---
+++ main.js
@@ -0,0 +1,11 @@
+const slider = document.querySelector('#range')
+
+const views = ['10K', '50K', '100K', '500K', '1M']
+const price = [8, 12, 16, 24, 36]
+
+
+slider.addEventListener('input', chengeSlider())
+function chengeSlider() {
+    const value = slider.value
+    slider.style.background = `linear-gradient(90deg, var(--cyan-100) ${slider[value]}%, var(--blue-200) ${slider[value]}%)`
+}
